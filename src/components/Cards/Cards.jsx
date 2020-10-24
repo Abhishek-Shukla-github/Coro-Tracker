d9239e3507c095ade2a58ae2b2a86b5df1f6070d
Abhishek Shukla <abhishekshuklaratedr@gmail.com>
2020-10-24 21:17:55 +0530

    Style the cards

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import { Grid, CardContent, Typography, Card } from "@material-ui/core";
 import styles from "./Cards.module.css";
 import CountUp from "react-countup";
+import cx from "classnames";
 
 const Cards = ({ data: {confirmed,deaths,recovered,lastUpdate}}) => {
     if (!confirmed) return <h1>Loading....</h1>
     return (
         <div className={styles.container}>
             <Grid container spacing={3} justify="center">
-            <Grid item component={Card}>
+                <Grid item component={Card} xs={12} md={3} className={cx(styles.card,styles.infected)}>
                 <CardContent>
                     <Typography color="textSecondary" gutterBottom>Infected</Typography>
                         <Typography variant="h5">
@@ -18,7 +19,7 @@ const Cards = ({ data: {confirmed,deaths,recovered,lastUpdate}}) => {
                     <Typography variant="body2">Number of active cases of COVID-19</Typography>
                 </CardContent>
             </Grid>
-            <Grid item component={Card}>
+            <Grid item component={Card} xs={12} md={3} className={cx(styles.card,styles.recovered)}>
                 <CardContent>
                     <Typography color="textSecondary" gutterBottom>Recovered</Typography>
                     <Typography variant="h5">
@@ -28,7 +29,7 @@ const Cards = ({ data: {confirmed,deaths,recovered,lastUpdate}}) => {
                     <Typography variant="body2">Number of recoveries from COVID-19</Typography>
                 </CardContent>
             </Grid>
-            <Grid item component={Card}>
+            <Grid item component={Card} xs={12} md={3} className={cx(styles.card,styles.deaths)}>
                 <CardContent>
                     <Typography color="textSecondary" gutterBottom>Death</Typography>
                     <Typography variant="h5">
