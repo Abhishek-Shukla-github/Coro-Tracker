8ea33763baf403ade4c9d97d7b2b56b3c853da36
Abhishek Shukla <abhishekshuklaratedr@gmail.com>
2020-10-24 12:05:35 +0530

    Add infected numbers and animate using CountUp

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import { Grid, CardContent, Typography, Card } from "@material-ui/core";
 import styles from "./Cards.module.css";
+import CountUp from "react-countup";
 
-const Cards = (props) => {
-    console.log(props);
+const Cards = ({ data: {confirmed,deaths,recovered,lastUpdate}}) => {
+    if (!confirmed) return <h1>Loading....</h1>
     return (
         <div className={styles.container}>
             <Grid container spacing={3} justify="center">
             <Grid item component={Card}>
                 <CardContent>
                     <Typography color="textSecondary" gutterBottom>Infected</Typography>
-                    <Typography variant="h5">Real Data</Typography>
+                        <Typography variant="h5">
+                            <CountUp start={0} end={confirmed.value} duration={2} separator={","} />
+                        </Typography>
                     <Typography color="textSecondary">Real Date</Typography>
                     <Typography variant="body2">Number of active cases of COVID-19</Typography>
                 </CardContent>
