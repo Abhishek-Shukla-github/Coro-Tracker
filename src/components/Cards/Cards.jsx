92d35adb827f7bdfe0b1ed4aa0c0d1d4e3a4cc67
Abhishek Shukla <abhishekshuklaratedr@gmail.com>
2020-10-24 11:38:05 +0530

    Add MUI Cards

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import React from 'react';
+import { Grid, CardContent, Typography, Card } from "@material-ui/core";
+import styles from "./Cards.module.css";
 
 const Cards = (props) => {
     console.log(props);
-    return <h1>Cards</h1>;
+    return (
+        <div className={styles.container}>
+            <Grid container spacing={3} justify="center">
+            <Grid item component={Card}>
+                <CardContent>
+                    <Typography color="textSecondary" gutterBottom>Infected</Typography>
+                    <Typography variant="h5">Real Data</Typography>
+                    <Typography color="textSecondary">Real Date</Typography>
+                    <Typography variant="body2">Number of active cases of COVID-19</Typography>
+                </CardContent>
+            </Grid>
+            <Grid item component={Card}>
+                <CardContent>
+                    <Typography color="textSecondary" gutterBottom>Recovered</Typography>
+                    <Typography variant="h5">Real Data</Typography>
+                    <Typography color="textSecondary">Real Date</Typography>
+                    <Typography variant="body2">Number of recoveries from COVID-19</Typography>
+                </CardContent>
+            </Grid>
+            <Grid item component={Card}>
+                <CardContent>
+                    <Typography color="textSecondary" gutterBottom>Death</Typography>
+                    <Typography variant="h5">Real Data</Typography>
+                    <Typography color="textSecondary">Real Date</Typography>
+                    <Typography variant="body2">Number of deaths due to COVID-19</Typography>
+                </CardContent>
+            </Grid>
+        </Grid>
+        </div>
+    );
 }
 
 export default Cards;
