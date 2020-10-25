cb0d1facfc228e48d7004feba3c0b5c35a5f20a2
Abhishek Shukla <abhishekshuklaratedr@gmail.com>
2020-10-25 16:14:48 +0530

    Add Line graph and change API code for daily death to only USA's daily death

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,7 +1,52 @@
-import React from 'react'
+import React, {useState,useEffect} from 'react';
+import { Line, Bar } from "react-chartjs-2";
+import styles from "./Chart.module.css";
+import { fetchDailyData } from '../../api';
+
 
 const Chart = () => {
-    return <h1>Chart</h1>;
+    const [dailyData, setDailyData] = useState([]);
+    useEffect(() => {
+        const fetchAPI = async () => {
+            const initialDailyData = await fetchDailyData();
+            setDailyData(initialDailyData);
+        }
+        fetchAPI();
+    },[dailyData])
+   
+   const lineChart = (
+    dailyData[0] ? (
+      <Line
+        data={{
+          labels: dailyData.map(({ date }) => new Date(date).toLocaleDateString()),
+          datasets: [{
+            data: dailyData.map((data) => data.confirmed),
+            label: 'Infected',
+            borderColor: '#3333ff',
+            fill: true,
+          }, {
+            data: dailyData.map((data) => data.deaths),
+            label: 'Deaths',
+            borderColor: 'red',
+            backgroundColor: 'rgba(255, 0, 0, 0.5)',
+            fill: true,
+          },  {
+            data: dailyData.map((data) => data.recovered),
+            label: 'Recovered',
+            borderColor: 'green',
+            backgroundColor: 'rgba(0, 255, 0, 0.5)',
+            fill: true,
+          },
+          ],
+        }}
+      />
+    ) : null
+  );
+
+
+    return (
+        <div className={styles.container}>{lineChart}</div>
+    );
 }
 
 export default Chart;
